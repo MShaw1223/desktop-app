76aeba711899df25ee9c9b3f6dfd8f664ecc8350
Miller <millershaw61@gmail.com>
2024-08-08 18:47:19 +0100

    Chore: Commenting
    Comments to clear up the use of callback and some of the language features seen in slint as well as new rust syntax

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui: AppWindow = AppWindow::new()?;
     let ui_handle: Weak<AppWindow> = ui.as_weak();
-
+    // on_ or invoke_ can be used on callbacks in the slint appwindow file in the main Window component
     ui.on_process_input({
         move |string: SharedString| {
             let ui: AppWindow = ui_handle.unwrap();
+            //set_ and get_ can be used on the properties of the main window component in the appwindow file
             ui.set_output(string.trim().into());
         }
     });
